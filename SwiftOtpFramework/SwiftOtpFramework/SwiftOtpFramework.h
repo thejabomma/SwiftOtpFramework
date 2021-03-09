3a0d9bc48ccf53bd726d9daf95de419b76b142c6
thejabomma <theja@biocipher.in>
2021-03-09 23:47:02 +0530

    fineshedframework

---
+++ SwiftOtpFramework/SwiftOtpFramework/SwiftOtpFramework.h
@@ -0,0 +1,18 @@
+//
+//  SwiftOtpFramework.h
+//  SwiftOtpFramework
+//
+//  Created by apple on 09/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftOtpFramework.
+FOUNDATION_EXPORT double SwiftOtpFrameworkVersionNumber;
+
+//! Project version string for SwiftOtpFramework.
+FOUNDATION_EXPORT const unsigned char SwiftOtpFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftOtpFramework/PublicHeader.h>
+
+
---
+++ SwiftOtpFramework/SwiftOtpFramework/SwiftOtpFramework.swift
@@ -0,0 +1,26 @@
+//
+//  SwiftOtpFramework.swift
+//  SwiftOtpFramework
+//
+//  Created by apple on 09/03/21.
+//
+
+import Foundation
+import UIKit
+
+
+public class MyNewFramework {
+    
+    public init() {}
+  
+    public func testFunction() {
+        print("This is test function")
+    }
+    
+    public func showAlert(message:String,viewController : UIViewController) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        viewController.present(alert, animated: true, completion: nil)
+    }
+    
+}
